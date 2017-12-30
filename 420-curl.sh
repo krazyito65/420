957a461652ad36d1554b257d89740914c8ca53dc
wabot <wabot@bot.rebuffguild.com>
2017-12-30 17:34:35 -0600

    using curl 1 line instead of dumb js script

---
+++ 420-curl.sh
@@ -0,0 +1,6 @@
+#{ "username": "420-test", "avatar_url": "https://i.ytimg.com/vi/Dkm8Hteeh6M/maxresdefault.jpg", "content" : "<@&250369565677060097> It's time." }
+
+
+
+curl -X POST --data '{ "username": "420", "avatar_url": "https://i.ytimg.com/vi/Dkm8Hteeh6M/maxresdefault.jpg", "content" : "<@&250369565677060097>. It is time." }' -H "Content-Type: application/json" https://discordapp.com/api/webhooks/396806639866478592/74Lsetp6Qk5kTO8QKNukH3zq0ciNAM4JrhT_8pqxUDD7PD-wepThHfWUhw29BdPJbdsi
+
